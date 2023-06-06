ad6d8afefc9ece091f58f15de2510e4727f36675
Imaiez <67430844+Imaiez@users.noreply.github.com>
2023-06-06 17:52:03 -0600

    Creation of program and grid, first step
    co-authored-by: VaGar91 <victor.ag71@gmail.com>
    co-authored-by: Alonso2105 <alonso.l99@gmail.com>

---
+++ game-of-life.rb
@@ -0,0 +1,12 @@
+class GameOfLife
+
+  # def initialize(rows, cols)
+  #   @rows = rows
+  #   @cols = cols
+    grid = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+    # ['0', '0', '0'], ['0', '0', '0']]
+    grid.to_a.each {|r| puts r.inspect}
+    # puts grid
+  # end
+end
+
