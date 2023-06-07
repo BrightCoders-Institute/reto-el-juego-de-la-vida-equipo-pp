af8bf0ec9d84f8bfafeda1d8c3420e5607aa1909
Imaiez <67430844+Imaiez@users.noreply.github.com>
2023-06-06 18:45:38 -0600

    Creating grid semi-automatic
    co-authored-by: VaGar91 <victor.ag71@gmail.com>
    co-authored-by: Alonso2105 <alonso.l99@gmail.com>

--- game-of-life.rb
+++ game-of-life.rb
@@ -1,12 +1,22 @@
-class GameOfLife
 
-  # def initialize(rows, cols)
-  #   @rows = rows
-  #   @cols = cols
-    grid = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-    # ['0', '0', '0'], ['0', '0', '0']]
-    grid.to_a.each {|r| puts r.inspect}
-    # puts grid
-  # end
+rows = 3
+columns = 3
+
+# Create an empty grid
+grid = Array.new(rows,0) { Array.new(columns,0) }
+
+# Accessing and updating grid elements
+# grid[0][0] = 1
+# grid[0][1] = 0
+grid[0][2] = 1
+grid[1][0] = 1
+# grid[1][1] = 0
+# grid[1][2] = 0
+# grid[2][0] = 0
+grid[2][1] = 1
+# grid[2][2] = 0
+
+# # Display the grid
+grid.each do |row|
+  puts row.join(" ")
 end
-
